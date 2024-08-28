5b01506787417d8b94d5f18866084b723f30ba6a
Panagiotis Kosmas <pkosmass@gmail.com>
2024-08-28 23:41:15 +0300

    seventh

---
+++ netlify/functions/data.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose.connect('mongodb+srv://pkosmass:PyO6QhNjBofAi5fP@cluster0.9g1qy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { 
+    useNewUrlParser: true, useUnifiedTopology: true 
+});
+
+const formResponseSchema = new Schema({
+    activityStep: String,
+    imageReality: String,
+    details: Array,
+    mouseMovements: Array,
+    gazeCoordinates: Array,
+    time: Number,
+    createdAt: { type: Date, default: Date.now }
+});
+
+const FormResponse = mongoose.model('FormResponse', formResponseSchema);
+
+exports.handler = async (event) => {
+    if (event.httpMethod === 'GET') {
+        try {
+            const { activityStep } = event.queryStringParameters;  // Get the query parameter
+            const users = await FormResponse.find({ activityStep: activityStep });
+            return {
+                statusCode: 200,
+                body: JSON.stringify(users),
+            };
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: error.message }),
+            };
+        }
+    }
+    return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'Not Found' }),
+    };
+};
