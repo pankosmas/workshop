caca64edc4cb6fdb9d1178c604144f6581467071
Panagiotis Kosmas <pkosmass@gmail.com>
2024-08-29 07:26:01 +0300

    none

--- public/js/result_functions.js
+++ public/js/result_functions.js
@@ -228,7 +228,7 @@ function getDetailsArray() {
     // Get the selected option's text
     var activityStepValue = getActivityStepValue();
 
-    const radio_choices = { Real: 0, Tampered: 0, Deepfake: 0 };
+    var radio_choices = { Real: 0, Tampered: 0, Deepfake: 0 };
 
     if (activityStepValue == 'step1') { return { Normal: radio_choices, Awareness: radio_choices, Lighting_Shadow: radio_choices, Edges_Blending: radio_choices, Context: radio_choices }; }
     else if (activityStepValue == 'step2') { return { Normal: radio_choices, Awareness: radio_choices, Heatmap: radio_choices, Spatial: radio_choices, Colour: radio_choices }; }
