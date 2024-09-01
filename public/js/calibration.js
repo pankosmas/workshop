c02564ae8525d04ec2c16d857c28574f8cf83a59
Panagiotis Kosmas <pkosmass@gmail.com>
2024-09-01 22:19:29 +0300

    123

--- public/js/calibration.js
+++ public/js/calibration.js
@@ -236,8 +236,8 @@ document.addEventListener('DOMContentLoaded', function() {
       // Check the width and set the background image accordingly
       if (width < 1600) {
           body.style.background = imageSmallScreen;
-          body.style.backgroundSize = 'cover'; /* Ensures the image covers the entire window */
-          body.style.backgroundPosition = 'contain'; /* Centers the image */
+          body.style.backgroundSize = 'contain'; /* Ensures the image covers the entire window */
+          body.style.backgroundPosition = 'center'; /* Centers the image */
           body.style.backgroundRepeat = 'no-repeat'; /* Prevents the image from repeating */
           body.style.backgroundAttachment = 'fixed'; /* Keeps the image fixed while content scrolls */
       } else {
