c44835e39fd9b6617060185afab5dd9d0f69929a
Panagiotis Kosmas <pkosmass@gmail.com>
2024-09-05 20:59:32 +0300

    1234

--- public/js/mouse.js
+++ public/js/mouse.js
@@ -5,8 +5,10 @@ let lastEventTime = null;
 
 // Capture mouse movement data
 if (step1 || step2 || step3 || step4 || step5 || step6 || step7 || step8) {
-    const currentTime = Date.now();
+   
     document.addEventListener('mousemove', (event) => {
+        const currentTime = Date.now();
+        
         if (lastEventTime !== null) { 
             const duration = currentTime - lastEventTime;
             window.mouseArray.push({"x": event.clientX, "y": event.clientY, "duration": duration});
@@ -49,28 +51,4 @@ if (step1 || step2 || step3 || step4 || step5 || step6 || step7 || step8) {
 }
 function saveMouseToArrays(activityNumber, array) {
     saveDatasetToLocal(`mouse-${activityNumber}`, array);
-}
-// Save and Load Data to and from local storage!
-function saveDatasetToLocal(filename, dataset) {
-    localStorage.setItem(filename, JSON.stringify(dataset));
-    step1 = false; /* step 1 */
-    step2 = false; /* step 2 */
-    step3 = false; /* step 3 */
-    step4 = false; /* step 4 */
-    step5 = false; /* step 5 */
-    step6 = false; /* step 6 */
-    step7 = false; /* step 7 */
-    step8 = false; /* step 8 */
-}
-// Load dataset from local storage
-function loadDatasetFromLocal(filename) {
-	const storedData = localStorage.getItem(filename);
-	if (storedData) {
-		const dataset = JSON.parse(storedData);
-		//console.log(`Dataset loaded from local storage with filename: ${filename}`);
-		return dataset;
-	} else {
-		console.error(`No dataset found in local storage with filename: ${filename}`);
-		return null;
-	}
 }
