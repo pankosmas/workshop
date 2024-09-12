714f4fae49e37b957fe31469c99de9aa63a587c2
Panagiotis Kosmas <pkosmass@gmail.com>
2024-09-12 16:00:18 +0300

    1234

--- public/js/results.js
+++ public/js/results.js
@@ -72,31 +72,4 @@ function aggregatePoints(gazeCoordinates, mouseCoordinates, data) {
         gazeCoordinates = gazeCoordinates.concat(record.gazeCoordinates);        
         mouseCoordinates = mouseCoordinates.concat(record.mouseCoordinates);        
     })
-    console.log(gazeCoordinates);
-}
-
-// ============================== Visualization No.1 plot gaze data points
-function plotAggregatedPoints(data) {
-    // Transform the dataset
-    const transformedData = data.map(({ x, y }) => ({ x, y }));
-    const finalData = rescaleGazeData(transformedData);
-    var canvas = document.getElementById('heatmap');
-    // Check if the canvas is available
-    if (canvas.getContext) {
-        // Get the 2D drawing context
-        var ctx = canvas.getContext('2d');
-        // Set the canvas dimensions (if necessary)
-        canvas.width = canvas.clientWidth;
-        canvas.height = canvas.clientHeight;
-    }
-    reshapeContent(ctx);
-    // Adjust color scale based on your data density if needed
-    const color = d3.scaleSequential(d3.interpolateYlOrRd).domain([0, 1]);
-    finalData.forEach(d => {
-        ctx.beginPath();
-        ctx.arc(d.x, d.y, 7, 0, Math.PI * 2, true);
-        ctx.fillStyle = "rgba(255, 0, 0, 0.8)";
-        //ctx.fillStyle = "rgba(255, 255, 0, 0.9)";
-        ctx.fill();
-    });
 }
