497ca9a037a649deca7054dc698296f6771e1533
Panagiotis Kosmas <79458219+pankosmas@users.noreply.github.com>
2025-01-20 00:00:57 +0200

    Update data.js

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-mongoose.connect('mongodb+srv://pkosmass:PyO6QhNjBofAi5fP@cluster0.9g1qy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { 
+mongoose.connect(process.env.MONGO_URI, { 
     useNewUrlParser: true, useUnifiedTopology: true 
 });
 
